34c47aa1bdd63b4162082c01ccd45c1f8fa5e4dc
ZahwaniFaradiba30 <inizwaa@gmail.com>
2026-09-22 18:25:13 +0700

    mempelajari JavaScript

---
+++ Praktikum_JavaScript/StudiKasus_1.js
@@ -0,0 +1,11 @@
+const namaSiswa = "Zahwani Faradiba Al Amri";
+const umur = 16;
+const kelas = "XI RPL 2";
+const sudahMembayar = true;
+const nomorTelepon = null;
+
+console.log(`Nama: ${namaSiswa}`);
+console.log(`Umur: ${umur} tahun`);
+console.log(`Kelas: ${kelas}`);
+console.log(`Sudah membayar: ${sudahMembayar}`);
+console.log(`Nomor Telepon: ${nomorTelepon}`);
---
+++ Praktikum_JavaScript/tipedata.js
@@ -0,0 +1,77 @@
+const namaDepan = "Zahwani Faradiba";
+const namaBelakang = "Al Amri";
+
+const namaLengkap = namaDepan + " " + namaBelakang;
+
+console.log(namaLengkap);
+
+const nama ="Zahwani Faradiba Al Amri";
+const kelas = "XI RPL 2";
+
+const pesan = `Nama: ${nama}, Kelas: ${kelas}`;
+
+console.log(pesan);
+
+const umur = 16;
+const nilaiRataRata = 87.5;
+const saldo = -50000;
+
+console.log(typeof umur);
+console.log(typeof nilaiRataRata);
+
+const harga = 15000;
+const jumlah = 3;
+
+const total = harga * jumlah;
+
+console.log(total);
+
+const sudahMembayar = true;
+const sedangLibur = false;
+
+const nilai = 85;
+const lulus = nilai >= 75;
+
+console.log(lulus);
+
+let nomorTelepon;
+
+console.log(nomorTelepon);
+
+const fotoProfil = null;
+
+console.log(nomorTelepon);
+console.log(fotoProfil);
+
+console.log(typeof nama);
+
+let beratBadan = 53.7;
+console.log(typeof beratBadan);
+
+let sudahMenikah = false;
+console.log(typeof sudahMenikah);
+
+let tinggiBadan;
+console.log(typeof tinggiBadan);
+
+let penghasilan = null;
+console.log(typeof penghasilan);
+
+console.log(nilai);
+console.log(typeof nilai);
+
+const umurTeks = String(umur);
+
+console.log(umurTeks);
+console.log(typeof umurTeks);
+
+console.log(Boolean(1));
+console.log(Boolean(0));
+console.log(Boolean("Halo"));
+console.log(Boolean(" "));
+
+const Nilai = Number("delapan puluh");
+
+if (Number.isNaN(nilai)) {
+    console.log("Nilai bukan angka yang valid");
+}
